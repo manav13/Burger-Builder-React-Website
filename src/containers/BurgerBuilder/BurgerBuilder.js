13c35d325a4c707025e294d3c34982a93daffb19
Manav13 <manavprajapati1398@gmail.com>
2019-06-14 20:45:14 +0530

    Created Layout, Component for project

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger Image will be here</div>
+                <div>Build Controls will be here</div>
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder;
