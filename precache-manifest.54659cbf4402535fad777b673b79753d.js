16acfd7204db9c5b3de5b2a05bae58b8f59ad46e
Manav13 <manavprajapati1398@gmail.com>
2019-06-17 19:05:17 +0530

    Updates

---
+++ precache-manifest.54659cbf4402535fad777b673b79753d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c89f8972018018ba6c56791e7a8a47",
+    "url": "/Burger-Builder-React-Website/index.html"
+  },
+  {
+    "revision": "c3e300814ad7f6b7a72e",
+    "url": "/Burger-Builder-React-Website/static/css/main.49df0145.chunk.css"
+  },
+  {
+    "revision": "b78768292e7d038ad956",
+    "url": "/Burger-Builder-React-Website/static/js/2.ff3ae0bd.chunk.js"
+  },
+  {
+    "revision": "c3e300814ad7f6b7a72e",
+    "url": "/Burger-Builder-React-Website/static/js/main.9272387d.chunk.js"
+  },
+  {
+    "revision": "d4241231a36bf702e755",
+    "url": "/Burger-Builder-React-Website/static/js/runtime~main.0c06f6fd.js"
+  }
+]);
