788caf7896340a8989acfae75c442cde2b123661
Manav13 <manavprajapati1398@gmail.com>
2019-06-20 09:14:31 +0530

    Added SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
-import styles from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import styles from './Layout.css';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <main className={styles.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    };
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    };
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        })
+    };
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
 
-export default layout;
+export default Layout;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import burgerLogo from '../../assets/images/133 burger-logo.png';
 import styles from './Logo.css';
 
 const logo = (props) => (
-    <div className={styles.Logo}>
+    <div className={styles.Logo} style={{height: props.height}}>
         <img src={burgerLogo} alt="myBurger"/>
     </div>
 );
